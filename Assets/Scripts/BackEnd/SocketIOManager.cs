93de5fd7af317297a8f1202183613a76b95489fb
GhanshyamKalola <ghanshyam.kalola@underpinservices.org>
2026-08-24 15:59:09 +0530

    B2B standard changes

--- Assets/Scripts/BackEnd/SocketIOManager.cs
+++ Assets/Scripts/BackEnd/SocketIOManager.cs
@@ -70,6 +70,7 @@ public class SocketIOManager : MonoBehaviour
 #if UNITY_WEBGL && !UNITY_EDITOR
         if (JSManager != null)
         {
+            JSManager.RegisterAuthTokenListener(gameObject.name, "ReceiveAuthToken");
             JSManager.SendCustomMessage("authToken");
         }
 #else
